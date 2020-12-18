422d51ba95955a33db38be1250ea1a71dd0a1a4b
Justin Cho <justch0616@gmail.com>
2020-12-18 12:16:36 -0600

    Fixed morphext extension

--- root/js/main.js
+++ root/js/main.js
@@ -1,6 +1,6 @@
 $("#js-rotating").Morphext({
-    animation: "fadeIn", // Overrides default "bounceIn"
-    separator: "|", // Overrides default ","
+    animation: "bounceIn", // Overrides default "bounceIn"
+    separator: ",", // Overrides default ","
     speed: 3000, // Overrides default 2000
     complete: function () {
         // Overrides default empty function
