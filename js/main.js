78cfb2ba19032dfe16ad2b37529041db9d79e4d1
Josiah Fowler <josiah.james.fowler@gmail.com>
2020-12-18 13:04:10 -0500

    landing page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$("#js-rotating").Morphext({
+    animation: "fadeIn", // Overrides default "bounceIn"
+    separator: "|", // Overrides default ","
+    speed: 3000, // Overrides default 2000
+    complete: function () {
+        // Overrides default empty function
+    }
+});
